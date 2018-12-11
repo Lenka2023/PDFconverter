9d0b13fbc9b9f17330d2dca20c7a02504f318cf7
Lena <lenka20231@gmail.com>
2018-12-11 14:54:54 +0200

    fix error

--- config/app.php
+++ config/app.php
@@ -176,8 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        
     ],
 
     /*
