1a4031b9f58b3dcf22583118ddef81bb61870038
Lena <lenka20231@gmail.com>
2018-12-23 00:32:55 +0200

    fix errors

--- converttohtml.php
+++ converttohtml.php
@@ -3,12 +3,16 @@
 include 'vendor/autoload.php';
 use Gufy\PdfToHtml\Config;
 // change pdftohtml bin location
-Config::set('pdftohtml.bin', 'C:/poppler-0.37/bin/pdftohtml.exe');
+
+\Gufy\PdfToHtml\Config::set('pdftohtml.bin', 'D:\Program Files\xampp\htdocs\myminilaravel.loc/poppler-0.68.0/bin/pdftohtml.exe');
+
+
 
 // change pdfinfo bin location
-Config::set('pdfinfo.bin', 'C:/poppler-0.37/bin/pdfinfo.exe');
+\Gufy\PdfToHtml\Config::set('pdfinfo.bin', 'D:\Program Files\xampp\htdocs\myminilaravel.loc/poppler-0.68.0/bin/pdfinfo.exe');
+$file=dirname(__FILE__) .'/resources/document.pdf';
 // initiate
-$pdf = new Gufy\PdfToHtml\Pdf('file.pdf');
+$pdf = new Gufy\PdfToHtml\Pdf($file);
 
 // convert to html and return it as [Dom Object](https://github.com/paquettg/php-html-parser)
 $html = $pdf->html();
@@ -17,9 +21,9 @@ $html = $pdf->html();
 $total_pages = $pdf->getPages();
 
 // Your pdf happen to have more than one pages and you want to go another page? Got it. use this command to change the current page to page 3
-$html->goToPage(3);
+//$html->goToPage(3);
 
 // and then you can do as you please with that dom, you can find any element you want
-$paragraphs = $html->find('body > p');
+//$paragraphs = $html->find('body > p');
 
 ?>
